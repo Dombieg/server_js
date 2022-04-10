ddab827e9689d7696f392777112264c71bc71427
Dombieg <101454919+Dombieg@users.noreply.github.com>
2022-04-10 09:47:20 +0200

    updated response

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const port = process.env.PORT || 9000;
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end('Dominik jest super SUPER!');
+  res.end('Dominik jest turbo super SUPER!');
 }
 
 const server = http.createServer(requestListener);
